9fb5a00e96dfa94ab90f7f44b6ea58b8a4500bb2
carla silva <carliitasilvaa@gmail.com>
2023-11-12 15:24:42 -0300

    entrega final

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
-let catalogo = [
-    {
-        titulo: "Barbie",
-        calificacion: 0 
-    },
-    {
-        titulo: "Saw X: El juego del miedo",
-        calificacion: 0
-    },
-    {
-        titulo: "Sonidos de libertad",
-        calificacion: 0
-    }
-];
-
-function buscarPorTitulo(titulo) {
-    return catalogo.find(item => item.titulo.toLowerCase() === titulo.toLowerCase());
-}
-
-function filtrarPorCalificacion(calificacionMinima) {
-    return catalogo.filter(item => item.calificacion >= calificacionMinima);
-}
-
 document.addEventListener("DOMContentLoaded", function() {
+    let bienvenido;
+    let catalogo = [];
+
+    function cargarCatalogo() {
+        return fetch('catalogo.json')
+            .then(response => response.json())
+            .then(data => {
+                catalogo = data;
+            })
+            .catch(error => console.error('Error al cargar el catálogo:', error));
+    }
+
+    // Llamar a la función para cargar el catálogo al cargar la página
+    cargarCatalogo().then(() =>{
+    function buscarPorTitulo(titulo) {
+        return catalogo.find(item => item.titulo.toLowerCase() === titulo.toLowerCase());
+    }
+
+    function filtrarPorCalificacion(calificacionMinima) {
+        return catalogo.filter(item => item.calificacion >= calificacionMinima);
+    }
+
     let formulario = document.getElementById("formulario");
     let planInput = document.getElementById("elijasuplan");
     let mesesInput = document.getElementById("elijalacantidaddemeses");
@@ -34,45 +33,44 @@ document.addEventListener("DOMContentLoaded", function() {
         let bienvenido = planInput.value.toLowerCase();
         let meses1 = parseInt(mesesInput.value);
 
-        if (bienvenido === "plan basico" || bienvenido === "plan intermedio" || bienvenido === "plan familiar") {
-            let costoTotal = calcularCostoDelPlan(bienvenido, meses1);
-            mensajeTitulo.textContent = `Costo total del Plan ${bienvenido} por ${meses1} meses: $${costoTotal}`;
-            mensajeTexto.textContent = ""; 
-        } else {
-            mensajeTitulo.textContent = "Plan incorrecto";
-            mensajeTexto.textContent = ""; 
-        }
+        let costoTotal = bienvenido === "plan basico" || bienvenido === "plan intermedio" || bienvenido === "plan familiar"
+        ? calcularCostoDelPlan(bienvenido, meses1)
+        : (mensajeTitulo.textContent = "Plan incorrecto", mensajeTexto.textContent = "", 0);
+    
+    if (costoTotal) {
+        mensajeTitulo.textContent = `Costo total del Plan ${bienvenido} por ${meses1} meses: $${costoTotal}`;
+    }
+    
     });
-});
 
 
-function calcularCostoDelPlan(bienvenido, meses1) {
-    let planB = 1500;
-    let planI = 2000;
-    let planF = 3000;
-    let costoTotal = 0;
+    function calcularCostoDelPlan(bienvenido, meses1) {
+        let planB = 1500;
+        let planI = 2000;
+        let planF = 3000;
+        let costoTotal = 0;
 
-    switch (bienvenido) {
-        case "plan basico":
-            costoTotal = meses1 * planB;
-            break;
-        case "plan intermedio":
-            costoTotal = meses1 * planI;
-            break;
-        case "plan familiar":
-            costoTotal = meses1 * planF;
-            break;
-        default:
-            alert("El plan seleccionado no es válido.");
-            return 0;
+        switch (bienvenido) {
+            case "plan basico":
+                costoTotal = meses1 * planB;
+                break;
+            case "plan intermedio":
+                costoTotal = meses1 * planI;
+                break;
+            case "plan familiar":
+                costoTotal = meses1 * planF;
+                break;
+            default:
+                alert("El plan seleccionado no es válido.");
+                return 0;
+        }
+
+        return costoTotal;
     }
 
-    return costoTotal;
-}
-document.addEventListener("DOMContentLoaded", function() {
     let continuarInput = document.createElement("input");
     continuarInput.type = "text";
-    continuarInput.placeholder = "¿Quieres contratar el plan? (sí/no), (calificar) para calificar las películas o (buscar) para buscar una película por título";
+    continuarInput.placeholder = "¿Quieres contratar el plan? (sí), (calificar) para calificar las películas o (buscar) para buscar una película por título";
     continuarInput.classList.add("mi-input");
     document.body.appendChild(continuarInput);
 
@@ -80,61 +78,155 @@ document.addEventListener("DOMContentLoaded", function() {
     botonEnviar.textContent = "Enviar";
     document.body.appendChild(botonEnviar);
 
-    botonEnviar.addEventListener("click", function() {
-        let continuar = continuarInput.value.toLowerCase();
-        if (continuar === "si") {
-            let emailInput = document.createElement("input");
-            emailInput.type = "email";
-            emailInput.placeholder = "Por favor, ingresa tu dirección de correo electrónico:";
-            document.body.appendChild(emailInput);
-
-            let botonEnviarEmail = document.createElement("button");
-            botonEnviarEmail.textContent = "Enviar";
-            document.body.appendChild(botonEnviarEmail);
-
-            botonEnviarEmail.addEventListener("click", function() {
-                let email = emailInput.value;
-                localStorage.setItem("email", email);
-                alert(`Gracias por utilizar Narex. Te hemos enviado un correo de confirmación a ${email}.`);
-            });
-        } else if (continuar === "calificar") {
-    for (let i = 0; i < catalogo.length; i++) {
-        let calificacion = parseInt(prompt(`Por favor, califica "${catalogo[i].titulo}" del 1 al 5:`));
-        catalogo[i].calificacion = calificacion;
-    }
-
-    alert("¡Gracias por tus calificaciones! Ahora podemos ofrecerte recomendaciones personalizadas.");
+botonEnviar.addEventListener("click",async function() {
+    let continuar = continuarInput.value.toLowerCase();
     
-    let deseaFiltrar = prompt("¿Deseas filtrar las películas por calificación mínima? (sí/no)").toLowerCase();
+    if (continuar === "si" && bienvenido !== "calificar") {
+        let emailInput = document.createElement("input");
+        emailInput.type = "email";
+        emailInput.placeholder = "Por favor, ingresa tu dirección de correo electrónico:";
+        document.body.appendChild(emailInput);
 
-    if (deseaFiltrar === "si") {
-        let calificacionMinima = parseInt(prompt("Ingresa la calificación mínima que deseas buscar:"));
-        let resultadosFiltrados = filtrarPorCalificacion(calificacionMinima);
+        let botonEnviarEmail = document.createElement("button");
+        botonEnviarEmail.textContent = "Enviar";
+        document.body.appendChild(botonEnviarEmail);
 
-        if (resultadosFiltrados.length > 0) {
-            let mensaje = "Películas/Series con calificación " + calificacionMinima + " o superior:\n";
-
-            for (let i = 0; i < resultadosFiltrados.length; i++) {
-                mensaje += `${resultadosFiltrados[i].titulo} - Calificación: ${resultadosFiltrados[i].calificacion}\n`;
-            }
-
-            alert(mensaje);
-        } else {
-            alert(`No hay películas/series con calificación ${calificacionMinima} o superior en el catálogo.`);
+        botonEnviarEmail.addEventListener("click", function() {
+            let email = emailInput.value;
+            localStorage.setItem("email", email);
+            swal.fire (`Gracias por utilizar Narex. Te hemos enviado un correo de confirmación a ${email}.`);
+        });
+    } else if (continuar === "calificar") {
+        try {
+            await calificarPeliculasConPromesas();
+            mostrarFiltrarPorCalificacion();
+        } catch (error) {
+            console.error(error);
         }
-    } else {
-        alert("¡Gracias por utilizar Narex! Esperamos que disfrutes de nuestro servicio.");
-    }
 
-} else if (continuar === "no") {
-    alert("¡Gracias por tu visita! Esperamos verte de nuevo pronto.");
-}else if (continuar === "buscar") {
-    let tituloBuscado = prompt("Ingresa el título de la película que deseas buscar:");
-    let resultadoBusqueda = buscarPorTitulo(tituloBuscado);
+    }else if (continuar === "buscar") {
+        
+        Swal.fire({
+            title: "Ingresa el título de la película o serie:",
+            input: "text",
+            inputAttributes: {
+                autocapitalize: "off"
+            },
+            showCancelButton: true,
+            confirmButtonText: "Buscar",
+            cancelButtonText: "Cancelar",
+            showLoaderOnConfirm: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+                let tituloABuscar = result.value;
 
-    if (resultadoBusqueda) {
-        alert(`Se encontró la película: ${resultadoBusqueda.titulo}`);
-    } else {
-        alert("No se encontró ninguna película con ese título en el catálogo.");
+                
+                let resultadoBusqueda = buscarPorTitulo(tituloABuscar);
+
+                if (resultadoBusqueda) {
+                    Swal.fire({
+                        title: "Resultado de la búsqueda",
+                        text: `${resultadoBusqueda.titulo} - Calificación: ${resultadoBusqueda.calificacion}`,
+                        icon: "success"
+                    });
+                } else {
+                    Swal.fire("Sin resultados", `No se encontró ninguna película o serie con el título "${tituloABuscar}".`, "warning");
+                }
+            }
+        });
     }
-} })})
+});
+    function calificarPeliculasConPromesas() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                for (let index = 0; index < catalogo.length; index++) {
+                    const result = await Swal.fire({
+                        title: `Por favor, califica "${catalogo[index].titulo}" del 1 al 5:`,
+                        input: "number",
+                        showDenyButton: true,
+                        showCancelButton: true,
+                        confirmButtonText: "Sí",
+                        denyButtonText: "No"
+                    });
+    
+                    if (result.isConfirmed) {
+                        let calificacion = parseInt(result.value);
+                        if (calificacion >= 1 && calificacion <= 5) {
+                            catalogo[index].calificacion = calificacion;
+                        } else {
+                            Swal.fire("Por favor, ingresa una calificación válida del 1 al 5.", "", "warning");
+                            index--; // Decrementamos el índice para repetir la iteración actual.
+                        }
+                    } else {
+                        Swal.fire("Has cancelado la calificación.", "", "info");
+                        reject("El usuario canceló la calificación.");
+                        return; // Importante salir de la función para evitar ejecución adicional.
+                    }
+                }
+    
+                Swal.fire("¡Gracias por tus calificaciones! Ahora podemos ofrecerte recomendaciones personalizadas.");
+                mostrarFiltrarPorCalificacion();
+                resolve(); // Resolvemos la promesa después de completar el bucle.
+            } catch (error) {
+                console.error(error);
+                reject(error);
+            }
+        });
+    }
+    
+
+        
+
+
+    function mostrarFiltrarPorCalificacion() {
+        Swal.fire({
+            title: "¿Deseas filtrar las películas por calificación mínima?",
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: "Sí",
+            denyButtonText: "No"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    title: "Ingresa la calificación mínima que deseas buscar:",
+                    input: "number",
+                    inputAttributes: {
+                        min: 0
+                    },
+                    showCancelButton: true,
+                    confirmButtonText: "Buscar",
+                    cancelButtonText: "Cancelar"
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        let calificacionMinima = parseInt(result.value);
+                        let resultadosFiltrados = filtrarPorCalificacion(calificacionMinima);
+
+                        if (resultadosFiltrados.length > 0) {
+                            let mensaje = "Películas/Series con calificación " + calificacionMinima + " o superior:\n";
+
+                            for (let i = 0; i < resultadosFiltrados.length; i++) {
+                                mensaje += `${resultadosFiltrados[i].titulo} - Calificación: ${resultadosFiltrados[i].calificacion}\n`;
+                            }
+
+                            Swal.fire({
+                                title: "Resultados de la búsqueda",
+                                text: mensaje,
+                                icon: "success"
+                            });
+                        } else {
+                            Swal.fire({
+                                title: "Sin resultados",
+                                text: `No hay películas/series con calificación ${calificacionMinima} o superior en el catálogo.`,
+                                icon: "warning"
+                            });
+                        }
+                    } else {
+                        Swal.fire("Operación cancelada", "", "info");
+                    }
+                });
+            } else if (result.isDenied) {
+                Swal.fire("¡Gracias por utilizar Narex! Esperamos que disfrutes de nuestro servicio.", "", "info");
+            }
+        });
+    }
+});})
